559f5508f8299e40ad79eebcc38f8bc64380b62c
Alexis Tyler <xo@wvvw.me>
2021-06-04 23:48:26 +0930

    fix: accept argument in error log

--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,6 @@
 export const log = {
-    error(message: string | Error) {
-        console.error(message instanceof Error ? message.message : message);
+    error(message: string | Error, ...args: any[]) {
+        console.error(message instanceof Error ? message.message : message, ...args);
     },
     info(message: string, ...args: string[]) {
         console.info(message, ...args);
